c91fed60a6703500b2f7af4ddc0b9a8465e4c158
rough340 <connectorsast@gmail.com>
2026-06-21 18:51:54 +0530

    Clean and finalize codebase for production: remove unused test files, development folders (extracted, scratch, source-files), temp crops, and unreferenced assets

--- test.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-html = "<html><body>"
-files = glob.glob("c:/Users/ASUS/Desktop/Desi chakra website/extracted/contents-docx/word/media/*")
-for f in files:
-    html += f'<img src="file:///{f}" width="200"><br>{f}<br>'
-html += "</body></html>"
-with open("c:/Users/ASUS/Desktop/Desi chakra website/test_images.html", "w") as f:
-    f.write(html)
